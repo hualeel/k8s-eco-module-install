b1022d00cd907d8225fad39ea5fc14d924ae7e83
hualeel <hualeel@sina.com>
2021-03-04 12:28:39 +0800

    Update helm3-install.sh

--- helm3-install.sh
+++ helm3-install.sh
@@ -7,3 +7,7 @@ mv linux-amd64/helm /usr/local/bin/helm
 #初始化helm chart repo
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
+
+#微软仓库（http://mirror.azure.cn/kubernetes/charts/）这个仓库强烈推荐，基本上官网有的chart这里都有。
+#阿里云仓库（https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts ）
+#官方仓库（https://hub.kubeapps.com/charts/incubator）官方chart仓库，国内有点不好使。
